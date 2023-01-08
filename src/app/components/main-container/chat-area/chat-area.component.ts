5034bfdaf83218116bfcd6bb6b3576c7b239baab
elbry <abdoelbry22@gmail.com>
2023-01-08 02:29:06 +0200

    day4

--- src/app/components/main-container/chat-area/chat-area.component.ts
+++ src/app/components/main-container/chat-area/chat-area.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-area',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-area.component.css']
 })
 export class ChatAreaComponent {
+  formSubmit(form:NgForm):void{
 
+  }
 }
